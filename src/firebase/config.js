56b82dc9ccdf81fa87f96cfb0988ee8ba64b8155
marshall chen <marshallchan666@outlook.com>
2021-06-15 05:26:12 +0800

    firebase set up

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBF45L-ZmBaAnXEi86Qdl4yZ-IcIN3-udo",
+    authDomain: "vue-firbase-m-blog.firebaseapp.com",
+    projectId: "vue-firbase-m-blog",
+    storageBucket: "vue-firbase-m-blog.appspot.com",
+    messagingSenderId: "359555569829",
+    appId: "1:359555569829:web:80f723744b928a706d154c",
+    measurementId: "G-GNNQP6JFWG"
+  };
+
+// init firebase
+
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export{ projectFirestore, timestamp }
