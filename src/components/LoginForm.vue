cd2ca6e3598a38171af3c5ce09cba63875c677e5
marshall chen <marshallchan666@outlook.com>
2021-06-15 16:19:12 +0800

    form done with switch between

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,6 +1,5 @@
 <template>
-  <form @submit.prevent="handleSubmit" class="justify-self-center space-y-8 w-6/12 mt-5 grid">
-        <input type="text" required placeholder="display name" v-model="displayName" class="block rounded-3xl text-gray-400 text-lg pl-4 py-2 border border-gray-200 focus:outline-none">
+   <form @submit.prevent="handleSubmit" class="justify-self-center space-y-8 w-6/12 mt-5 grid">
         <input type="email" required placeholder="email" v-model="email" class="block rounded-3xl text-gray-400 text-lg pl-4 py-2 border border-gray-200 focus:outline-none">
         <input type="password" required placeholder="password" v-model="password" class="block rounded-3xl text-gray-400 text-lg pl-4 py-2 border border-gray-200 focus:outline-none">
         <button class="justify-self-center block bg-green-400 px-5 py-2 text-md font-medium rounded-full text-white text-center">Sign up</button>
@@ -11,14 +10,13 @@
 import { ref } from '@vue/reactivity'
 export default {
     setup() {
-        const displayName = ref('')
         const email = ref('')
         const password = ref('')
 
         const handleSubmit = () => {
             
         }
-        return { displayName, email, password, handleSubmit}
+        return { email, password, handleSubmit}
     }
 }
 </script>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,15 +1,31 @@
 <template>
-  <div>
-        <p>welcome</p>
-        <SignupForm />
+  <div class="bg-white justify-self-center w-3/5 p-10 rounded-xl shadow-lg mt-20 text-center grid">
+        <p class="text-xl font-semibold tracking-wider mb-5 text-gray-500">Welcome</p>
+        <div v-if="showLogin" class="grid">
+            <p class="text-3xl font-bold tracking-wider text-gray-600 mb-3">Login</p>
+            <LoginForm />
+            <p class="mt-5 text-sm text-gray-500">No account yet?<span @click="showLogin = false" class="font-bold text-lg underline px-2 cursor-pointer">Signup</span>instead</p>
+        </div>
+        <div v-else class="grid">
+            <p class="text-3xl font-bold tracking-wider text-gray-600 mb-3">Sign up</p>
+           <SignupForm />
+           <p class="mt-5 text-sm text-gray-500">Already registered<span @click="showLogin = true" class="font-bold text-lg underline px-2 cursor-pointer">Login</span>instead</p>
+        </div>
   </div>
 </template>
 
 <script>
 import SignupForm from '../components/SignupForm.vue'
+import LoginForm from '../components/LoginForm.vue'
+import { ref } from '@vue/reactivity'
 
 export default {
-    components: { SignupForm }
+    components: { SignupForm, LoginForm},
+    setup() {
+        const showLogin =ref(true)
+
+        return { showLogin }
+    } 
 }
 </script>
 
