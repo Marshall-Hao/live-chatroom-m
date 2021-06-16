08f9665f299a0cc385bfbb840e19f3d7040512a0
marshall chen <marshallchan666@outlook.com>
2021-06-16 18:01:15 +0800

    config the deploy setup, do some minor style change, and favicon title

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -4,7 +4,7 @@
         <div v-if="documents" ref="message" class="space-y-3 bg-gray-50 rounded-xl py-6 px-2 mt-5 h-96 md:h-80 overflow-y-auto">
             <div v-for="doc in formatteDocuments" :key="doc.id" class="block">
                 <span class="text-sm text-gray-400 font-medium">{{ doc.createdAt }}</span>
-                <div class="flex">
+                <div class="flex items-center">
                     <span class="inline-block text-lg font-semibold tracking-wide mr-3">{{ doc.name }}</span>
                     <span class="inline-block text-md text-base font-medium text-gray-700 overflow-ellipsis break-all">{{ doc.message }}</span>
                 </div>
